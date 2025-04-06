03945ed9ef501fd1f7b3457f27bc9af221548dd7
JagritGumber <gumberjagrit@gmail.com>
2025-04-07 03:17:00 +0530

    Initialized capacitor instead

---
+++ client/android/app/src/main/java/com/jagrit/autodisposal/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jagrit.autodisposal;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jagrit.autodisposal',
+  appName: 'autodisposal',
+  webDir: 'dist'
+};
+
+export default config;
